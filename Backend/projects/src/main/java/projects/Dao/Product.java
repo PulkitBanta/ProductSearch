2e6053701d584f723328496feba84cb419736d8c
Pulkit <Pulkitbanta55555@gmail.com>
2020-06-20 19:21:18 +0530

    removed unwanted code

--- Backend/projects/src/main/java/projects/Dao/Product.java
+++ Backend/projects/src/main/java/projects/Dao/Product.java
@@ -6,9 +6,7 @@ public class Product {
 	private String description;
 	private int price;
 	
-	public Product() {
-			
-	}
+	public Product() { }
 	
 	public Product(String image_url, String name, String description, int price) {
 		this.image_url = image_url;
--- Backend/projects/src/main/java/projects/service/ProductService.java
+++ Backend/projects/src/main/java/projects/service/ProductService.java
@@ -1,7 +1,6 @@
 package projects.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
