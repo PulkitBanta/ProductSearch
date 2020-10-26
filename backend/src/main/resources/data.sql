f0ee07b3d73cdfb1b5a6be173dfbd3e6fcd6dc17
Pulkit <Pulkitbanta55555@gmail.com>
2020-10-26 22:22:07 +0530

    update: data, not found

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO PRODUCT (id, image_url, name, description, price) VALUES
-(1, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3300', 'Ryzen CPU with 6 cores, 12 threads', 12000),
-(2, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3100', 'Ryzen CPU with 6 cores, 12 threads', 13000),
-(3, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3600', 'Ryzen CPU with 6 cores, 12 threads', 14000),
-(4, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3700', 'Ryzen CPU with 6 cores, 12 threads', 15000),
-(5, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3800', 'Ryzen CPU with 6 cores, 12 threads', 16000),
-(6, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3900', 'Ryzen CPU with 6 cores, 12 threads', 17000);
+(1, 'https://cdn.pixabay.com/photo/2020/03/20/22/29/amd-4952189_1280.jpg', 'Ryzen 3300', 'Ryzen CPU with 6 cores, 12 threads', 21000),
+(2, 'https://cdn.pixabay.com/photo/2020/02/02/13/55/memory-4813085_1280.jpg', '32 GB DDR4 sodimm Ram', '32 GB high speed sodimm ram for laptop, frequency 2666Mhz', 16000),
+(3, 'https://cdn.pixabay.com/photo/2012/02/28/00/52/blank-17894_1280.jpg', 'ABC monitor', 'High speed monitor, 3ms response time and 144Hz refresh rate. Best monitor for gaming as well as productivity.', 14000),
+(4, 'https://cdn.pixabay.com/photo/2014/04/09/08/16/data-storage-319844_1280.jpg', 'Sandisk SSD', 'High Speed Solid State Drive. Read and write speed upto 500mbps. Capacity 1TB', 9645),
+(5, 'https://cdn.pixabay.com/photo/2020/06/07/00/19/cyber-monday-5268589_1280.jpg', 'GTX 1080Ti', 'Best budget graphics card for gaming as well as productivty', 20000),
+(6, 'https://cdn.pixabay.com/photo/2020/06/06/01/03/nvidia-5264914_1280.jpg', 'RTX 2080 Super', 'High end graphics card for gaming and productivity, one of the top class graphics card with ray tracing and other RTX eclusive features.', 17000);
